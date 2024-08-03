7f10d2babb95797f5661fe0a5274f7f9d8bc192c
zhengyansheng <zhengyscn@gmai.com>
2024-08-03 17:28:55 +0800

    gen webhook

--- cmd/main.go
+++ cmd/main.go
@@ -151,6 +151,12 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Frigate")
 		os.Exit(1)
 	}
+	if os.Getenv("ENABLE_WEBHOOKS") != "false" {
+		if err = (&shipv1beta1.Frigate{}).SetupWebhookWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create webhook", "webhook", "Frigate")
+			os.Exit(1)
+		}
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
